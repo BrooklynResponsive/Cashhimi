5b1d3b8e42971451c93f386ab7977f7db37eadeb
Matt Lima <mlima@bizbash.com>
2013-08-22 14:30:10 -0400

    First stab at template include structure, integration of push menu and foundation.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Cashhimi Home</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="shortcut icon" href="favicon.ico">
+    
+
+  <? require('include/all_pages_head_element.inc'); ?>
+
+
+
+
+
+
+
+</head>
+<body>
+  
+  
+  <? 
+  	//The header and body need to be done by the same include because of the push menu.
+  	require("include/all_pages_body.inc"); ?>
+
+  
+
+</body>
+
+    <? require('include/all_pages_before_html_end.inc'); ?>
+
+
+</html>
