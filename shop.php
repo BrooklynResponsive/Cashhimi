f47f184644883b1fb96b0aab99c699a7e237935a
Matt Lima <mlima@bizbash.com>
2013-08-23 16:32:23 -0400

    First draft of all pages

---
+++ shop.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Cashhimi Shop</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="shortcut icon" href="favicon.ico">
+    
+
+  <? require('include/all_pages_head_element.inc'); ?>
+
+
+
+
+
+
+
+</head>
+<body>
+  
+  
+  <? 
+  	//The header and body need to be done by the same include because of the push menu.
+  	require("include/all_pages_body.inc"); ?>
+
+  
+
+</body>
+
+    <? require('include/all_pages_before_html_end.inc'); ?>
+	
+	<script>
+		$(document).ready(function() { 
+			$(".products-slider").foundation('orbit',{
+			  animation: 'slide',
+/* 			  timer_speed: 10000, */
+			  pause_on_hover: true,
+			  resume_on_mouseout: false,
+			  animation_speed: 500,
+			  stack_on_small: false,
+			  navigation_arrows: true,
+			  slide_number: true,
+			  container_class: 'orbit-container',
+/* 			  stack_on_small_class: 'orbit-stack-on-small', */
+			  next_class: 'orbit-next',
+			  prev_class: 'orbit-prev',
+/* 			  timer_container_class: 'orbit-timer', */
+/* 			  timer_paused_class: 'paused', */
+/* 			  timer_progress_class: 'orbit-progress', */
+			  slides_container_class: 'orbit-slides-container',
+			  bullets_container_class: 'orbit-bullets',
+			  bullets_active_class: 'active',
+			  slide_number_class: 'orbit-slide-number-products',
+			  caption_class: 'orbit-caption',
+			  active_slide_class: 'active',
+			  orbit_transition_class: 'orbit-transitioning',
+			  bullets: false,
+			  timer: false,
+			  variable_height: false,
+			  before_slide_change: function(){ $(".swipe-for-more").remove(); }
+/* 			  after_slide_change: function(){} */
+			})	
+		});
+		
+	</script>
+
+</html>
